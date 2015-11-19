d0aaab1df085d0e2534f2b07902efdfdc9f2180f
LiuWenJu <lwj037303@gmail.com>
2015-11-19 22:59:47 +0800

    a binary search

---
+++ somecode/binary_search.py
@@ -0,0 +1,28 @@
+#coding=utf-8
+
+def binarySearch(l, t):
+    low, high = 0, len(l) - 1
+    while low < high:
+        print low, high
+        mid = (low + high) / 2
+        if l[mid] > t:
+            high = mid
+        elif l[mid] < t:
+            low = mid + 1
+        else:
+            return mid
+    return False
+
+if __name__ == '__main__':
+    l = [1, 3, 12, 45, 66, 99, 120, 444]
+    print binarySearch(l, 12)
+    print binarySearch(l, 1)
+    print binarySearch(l, 13)
+
+
+
+
+
+
+#for i in range(10, 0, -1):
+#    print '\t', i, '!'
